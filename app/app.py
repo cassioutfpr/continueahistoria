319f838ba14d430a453dfb9e3665b715c1925caf
Cassio Morales <cassioutfpr@gmail.com>
2021-01-01 10:22:49 -0300

    Added login and logout functionality.

--- app/app.py
+++ app/app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from typing import List, Dict
+from flask_login import LoginManager
+from auth import auth
+from main import main
+from models import User
 import mysql.connector
 import json
 import uuid
@@ -17,108 +21,65 @@ config = {
 
 #settings
 app.secret_key = 'mysecretkey'
-numberOfUsers = 0
 
-def getUsers() -> List[Dict]:
-	global numberOfUsers
+app.register_blueprint(auth)
+app.register_blueprint(main)
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
 	connection = mysql.connector.connect(**config)
 	cursor = connection.cursor()
-	cursor.execute('SELECT * FROM Users')
+	cursor.execute('SELECT * FROM Users WHERE id = %s', (user_id,))
 	data = cursor.fetchall()
-	#results = [{id: name} for (id, name) in cursor]
-	numberOfUsers = len(data)
-	cursor.close()
-	connection.close()
+	if data:
+		#vamos evitar magic numbers, por favor
+		return User(data[0][0], data[0][1], data[0][2], data[0][3])
+	else:
+		return None
 
-	return data
-
-@app.route('/')
-def Index():
-	print(getUsers())
-	return render_template('index.html', users = getUsers())
-
-@app.route('/login')
-def Login():
-	return render_template('login.html')
-
-@app.route('/login_user', methods=['POST'])
-def login_user():
-	if request.method == 'POST':
-		email = request.form['email']
-		password = request.form['password']  #HASH IT FIRST
-
-		connection = mysql.connector.connect(**config)
-		cursor = connection.cursor()
-		cursor.execute('SELECT * FROM Users WHERE email = %s AND password = %s', (email, password))
-		data = cursor.fetchall()
-		print(data)
-		#if data null ....
-
-		return redirect(url_for('Index'))
-
-@app.route('/register')
-def Register():
-	return render_template('register.html')
-
-@app.route('/add_user', methods=['POST'])
-def add_user():
-	if request.method == 'POST':
-		#TODO: check password and confirm password
-		#return to front
-
-		user = request.form['user']
-		email = request.form['email']
-		password = request.form['password']  #HASH IT FIRST
-	
-		connection = mysql.connector.connect(**config)
-		cursor = connection.cursor()
-		
-		#check if email and username exist
-		#return to front if they do
-		cursor.execute('INSERT INTO Users (id, name, email, password) VALUES (%s, %s, %s, %s)', (str(uuid.uuid4().hex), user, email, password))
-		
-		connection.commit()
-		flash('Contact Added succesfully')
-		cursor.close()
-		connection.close()
-	
-
-		return redirect(url_for('Index'))
-
-@app.route('/getUsers')
-def get_contacts():
-	return json.dumps({'usuarios': getUsers()})
-
-@app.route('/delete/<string:id>')
-def delete_contact(id):
-	connection = mysql.connector.connect(**config)
-	cursor = connection.cursor()
-	cursor.execute('DELETE FROM Users WHERE ID = {0}'.format(id))
-	connection.commit()
-	flash('Contato removido')
-	return redirect(url_for('Index'))
-
-@app.route('/edit/<id>')
-def edit_contact(id):
-	connection = mysql.connector.connect(**config)
-	cursor = connection.cursor()
-	cursor.execute('SELECT * FROM Users WHERE ID = %s', (id,))
-	data = cursor.fetchall()
-	return render_template('edit-contact.html', user = data[0])
-
-@app.route('/update/<id>', methods = ['POST'])
-def update(id):
-	if request.method == 'POST':
-		name = request.form['name']
-		connection = mysql.connector.connect(**config)
-		cursor = connection.cursor()
-		cursor.execute("""
-			UPDATE Users
-			SET name = %s
-			WHERE ID = %s
-		 """, (name, id))
-		connection.commit()
-	return redirect(url_for('Index'))
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
+
+
+
+
+##UNUSED METHODS##
+# @app.route('/getUsers')
+# def get_contacts():
+# 	return json.dumps({'usuarios': getUsers()})
+
+# @app.route('/delete/<string:id>')
+# def delete_contact(id):
+# 	connection = mysql.connector.connect(**config)
+# 	cursor = connection.cursor()
+# 	cursor.execute('DELETE FROM Users WHERE ID = {0}'.format(id))
+# 	connection.commit()
+# 	flash('Contato removido')
+# 	return redirect(url_for('Index'))
+
+# @app.route('/edit/<id>')
+# def edit_contact(id):
+# 	connection = mysql.connector.connect(**config)
+# 	cursor = connection.cursor()
+# 	cursor.execute('SELECT * FROM Users WHERE ID = %s', (id,))
+# 	data = cursor.fetchall()
+# 	return render_template('edit-contact.html', user = data[0])
+
+# @app.route('/update/<id>', methods = ['POST'])
+# def update(id):
+# 	if request.method == 'POST':
+# 		name = request.form['name']
+# 		connection = mysql.connector.connect(**config)
+# 		cursor = connection.cursor()
+# 		cursor.execute("""
+# 			UPDATE Users
+# 			SET name = %s
+# 			WHERE ID = %s
+# 		 """, (name, id))
+# 		connection.commit()
+# 	return redirect(url_for('Index'))
---
+++ app/auth.py
@@ -0,0 +1,87 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_user, logout_user, login_required
+from typing import List, Dict
+from models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+import mysql.connector
+import uuid
+
+auth = Blueprint('auth', __name__)
+
+config = {
+	'user': 'root',
+	'password': 'root',
+	'host': 'db',
+	'port': '3306',
+	'database': 'Cah'
+}
+
+@auth.route('/login')
+def login():
+	return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def loginPost():
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password'] 
+
+		connection = mysql.connector.connect(**config)
+		cursor = connection.cursor()
+		cursor.execute('SELECT * FROM Users WHERE email = %s', (email,))
+		data = cursor.fetchall()
+
+		if not data:
+			flash('Credenciais erradas')
+			return redirect(url_for('auth.login'))
+
+		if not check_password_hash(data[0][3], password):
+			flash('Credenciais erradas')
+			return redirect(url_for('auth.login'))
+
+
+		#vamos evitar magic numbers, por favor
+		login_user(User(data[0][0], data[0][1], data[0][2], data[0][3]))
+
+		return redirect(url_for('main.index'))
+
+@auth.route('/signup')
+def signup():
+	return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signupPost():
+	if request.method == 'POST':
+		#TODO: check if password and confirm password are the same
+		#return to front
+
+		user = request.form['user']
+		email = request.form['email']
+		password = request.form['password']  #HASH IT FIRST
+	
+		connection = mysql.connector.connect(**config)
+		cursor = connection.cursor()
+
+		cursor.execute('SELECT * FROM Users WHERE email = %s', (email,))
+		data = cursor.fetchall()
+
+		if data:
+			flash('Email já cadastrado.')
+			return redirect(url_for('auth.signup'))
+
+		hashed_password = generate_password_hash(password, method='sha256')
+		cursor.execute('INSERT INTO Users (id, name, email, password) VALUES (%s, %s, %s, %s)', (str(uuid.uuid4().hex), user, email, hashed_password))
+		
+		connection.commit()
+		flash('Cadastro realizado com sucesso')
+		cursor.close()
+		connection.close()
+	
+
+		return redirect(url_for('main.index'))
+
+@auth.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	return redirect(url_for('main.index'))
